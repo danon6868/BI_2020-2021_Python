8da489b5b33318778703ba1a32a2a3a2b081d9fa
Daniil Litvinov <48359219+danon6868@users.noreply.github.com>
2021-04-25 18:36:06 +0300

    Update dna_rna_tests.py

--- tests/dna_rna_tests/dna_rna_tests.py
+++ tests/dna_rna_tests/dna_rna_tests.py
@@ -1,6 +1,7 @@
 import unittest
-from dna_rna_classes import *
-
+from dna_rna_classes import RNA
+from dna_rna_classes import DNA
+from dna_rna_classes import Sequence
 
 class TestClassDNA(unittest.TestCase):
 
@@ -12,7 +13,7 @@ class TestClassDNA(unittest.TestCase):
         dna = DNA("GCC")
         rna = RNA("GCC")
         self.assertRaises(TypeError, dna, rna)
-        
+
     def test_str(self):
         dna = DNA("AATTGGCC")
         self.assertEqual(dna.seq, "AATTGGCC")
@@ -51,11 +52,11 @@ class TestClassDNA(unittest.TestCase):
     def test_dna_iterable(self):
         dna = DNA("AATTGGCC")
         self.assertListEqual([x for x in dna.seq], ["A", "A", "T", "T", "G", "G", "C", "C"])
-        
+
     def test_dna_hash(self):
         dna = DNA("agg")
         self.assertEqual(dna, DNA("AGg"))
-        
+
     def test_dna_slice(self):
         dna = DNA("AAgg")
         self.assertEqual(dna[2:4], DNA("gg"))
@@ -65,24 +66,24 @@ class TestClassRNA(unittest.TestCase):
     def test_rna_is_rna(self):
         rna = RNA("AAUUGGCC")
         self.assertEqual(type(rna), RNA)
-        
+
     def test_rna_is_not_dna(self):
         dna = DNA("GCC")
         rna = RNA("GCC")
         self.assertRaises(TypeError, rna, dna)
-        
+
     def test_str(self):
         rna = RNA("AAUUGGCC")
         self.assertEqual(rna.seq, "AAUUGGCC")
-    
+
     def test_rna_is_a_sequence(self):
         rna = RNA("AAUUGGCC")
         self.assertTrue(issubclass(type(rna), Sequence))
-        
+
     def test_rna_sequence_length(self):
         rna = RNA("AAUUGGCC")
         self.assertEqual(len(rna), 8)
-        
+
     def test_rna_gc_content(self):
         rna = RNA("AUGC")
         self.assertEqual(rna.gc_content(), 0.5)
@@ -90,30 +91,30 @@ class TestClassRNA(unittest.TestCase):
         self.assertEqual(rna.gc_content(), 0)
         rna = RNA("GCGCGCGC")
         self.assertEqual(rna.gc_content(), 1)
-        
+
     def test_rna_gc_content_zero_sequence_len(self):
         rna = RNA("")
         self.assertEqual(rna.gc_content(), None)
-        
+
     def test_rna_reverse_complement(self):
         rna = RNA("AAUUGGCC")
         self.assertEqual(rna.reverse_complement(), RNA("GGCCAAUU"))
-    
+
     def test_rna_equal(self):
         self.assertEqual(RNA("AAUUGGCC"), RNA("AAUUGGCC"))
 
     def test_rna_iterable(self):
         rna = RNA("AAUUGGCC")
         self.assertListEqual([x for x in rna.seq], ["A", "A", "U", "U", "G", "G", "C", "C"])
-        
+
     def test_rna_hash(self):
         rna = RNA("agg")
         self.assertEqual(rna, RNA("AGg"))
-        
+
     def test_rna_slice(self):
         rna = RNA("AAgg")
         self.assertEqual(rna[2:4], RNA("gg"))
-        
-        
+
+
 if __name__ == "__main__":
     unittest.main()
